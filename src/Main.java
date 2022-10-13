270a26d2f437ec2890f88e8c29219d3336704cd5
jonathan <87816454+jona199608@users.noreply.github.com>
2022-10-12 23:04:28 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // usando if--------------------------------------------------
+
+//        var numeroIf = -4;
+//        if (numeroIf > 0){
+//            System.out.println(numeroIf + 0 + " positivo");
+//        } else if (numeroIf < 0) {
+//            System.out.println(numeroIf + 0 + " negativo");
+//        } else
+//            System.out.println(numeroIf + 0 +" es 0");
+
+    // usando while-----------------------------------------------
+
+        int numeroWhile = 1;
+        while (numeroWhile <= 3){
+            System.out.println(numeroWhile);
+            numeroWhile ++;
+        }
+
+    // usando Do while---------------------------------------------
+
+//       int contador = 1;
+//        do {
+//              System.out.println(contador);
+//              contador++;
+//       } while (contador >= 3);
+
+    // usando for--------------------------------------------------
+
+//        int numeroFor = 0;
+//        for (int i; numeroFor <= 3 ; numeroFor++){
+//            System.out.println(numeroFor);
+//        }
+
+    // usando Switch----------------------------------------------
+
+//        var estacion = "otoño";
+//        switch (estacion){
+//            case "verano":
+//            case "otoño":
+//            case "invierno":
+//            case "primavera":
+//                System.out.println("la estacion del año es: "+ estacion);
+//                break;
+//            default:
+//                System.out.println("nose encontro estacion");
+//                break;
+//        }
+
     }
 }
